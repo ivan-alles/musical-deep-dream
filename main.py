f0b3d8e5ee9697c1b8fd81000d92ac172ab36548
Ivan Alles <ivan.alles@gmail.com>
2021-05-04 19:03:53 +0200

    Experiments with parameters.

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ file_name = 'songs/metal.00000.wav'
 model = 'MSD_musicnn'
 # model = 'MSD_vgg'
 
-taggram, tags, feature_map = extractor(file_name, model=model, input_length=0.1)
+taggram, tags, feature_map = extractor(file_name, model=model, input_length=1)
 
 # Features with time axis: mean_pool, max_pool, penultimate
 
 print(feature_map.keys())
 
-song_features = feature_map['max_pool']
+song_features = feature_map['mean_pool']
 
 import cv2
 import numpy as np
@@ -23,7 +23,7 @@ import tensorflow as tf
 
 imagenet_mean = 117.0
 layer_names = ['mixed3a', 'mixed4a', 'mixed4e', 'mixed5b']
-layer_weights = [1, 1, 1, 1]
+layer_weights = [3, 4, 2, 1]
 
 rng = np.random.RandomState(1)
 
@@ -63,7 +63,7 @@ def deep_dream(model_path):
 
     learning_rate = 2
     for epoch in range(len(song_features)):
-        size = 7
+        size = 8
         # image = rng.uniform(imagenet_mean - 5, imagenet_mean + 5, (size, size, 3))
         image = np.full((size, size, 3), imagenet_mean, dtype=np.float32)
 
@@ -71,16 +71,12 @@ def deep_dream(model_path):
         features = song_features[epoch]
         features = cv2.resize(np.tile(features, (11, 1)), (num_features, 11), interpolation=cv2.INTER_LINEAR)[5]
         features = (features > 0.2).astype(np.float32)
+        print(f'Non-zero features {features.sum() / len(features) * 100:0.1f}%')
+        np.random.RandomState(1).shuffle(features)
         start = 0
         for l in range(len(layers)):
             layer = layers[l]
             target_size = int(layer.shape[3])
-            # if True:
-            #     t = rng.uniform(10, 100, target_size)
-            # else:
-            #     t = rng.uniform(10, 100, int(target_size * 0.1))
-            #     t = np.hstack((t, np.full(target_size - t.shape[0], 0.00001)))
-            #     rng.shuffle(t)
             t = features[start:start+target_size]
             start += target_size
             target_values.append(t)
@@ -100,8 +96,8 @@ def deep_dream(model_path):
                 view_image = np.power(view_image, 1.5)
                 cv2.imshow(f'result-{size}', view_image)
                 cv2.waitKey(1)
-            size *= 2
-            image = cv2.resize(image, (size, size), interpolation=cv2.INTER_CUBIC)
+            size *= 1.4
+            image = cv2.resize(image, (int(size), int(size)), interpolation=cv2.INTER_CUBIC)
 
 
 if __name__ == "__main__":
